7ce6e429b8f18853ab44b4bd5b7bf7d0df78c4e0
Guli.Ahmadova <ahmedovaguli2001@gmail.com>
2024-06-27 09:18:59 +0400

    handleSubmit func

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,7 +1,8 @@
 const API = {
   "login": "/auth/login",
   "otp_trust": "Auths/otp-trust",
-  "resend_otp": "Auths/resend-otp"
+  "resend_otp": "Auths/resend-otp",
+  "reset_password": "Auths/reset-password?token="
 };
 
 export default API;
