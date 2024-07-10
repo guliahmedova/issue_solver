a9800f1ffd48b1a094459e44036a5ae5a2750eb2
Guli.Ahmadova <ahmedovaguli2001@gmail.com>
2024-07-10 16:03:08 +0400

    not found page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,9 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {
@@ -22,37 +23,6 @@ const config: Config = {
       'xl': '1280px',
       'xxl': '1536px',
     }
-  },
-  colors: {
-    'white': '#ffffff',
-    'black': '#000000',
-    'red': '#ff0000',
-    'orange-primary': '#FF7900',
-    'orange-secondary': '#FFA500',
-    'blue-primary': '#2981FF',
-    'purple': '#7e5bef',
-    'pink': '#ff49db',
-    'orange': '#ff7849',
-    'green': '#13ce66',
-    'yellow': '#ffc82c',
-    'gray-dark': '#273444',
-    'gray': '#8492a6',
-    'gray-light': '#d3dce6',
-    'surface-secondary': '#9D9D9D',
-    'gray-disabled': '#9D9D9D26',
-    'text-gray': '#8C8C8C',
-    'parag-gray': '#6E6E6E',
-    'warning': '#C46017',
-    'surface-background': '#F0F4F9'
-  },
-  fontSize: {
-    xxs: ['13px', '16px'],
-    xs: ['15', '18px'],
-    sm: ['15px', '21px'],
-    'sm-alt': ['15px', '18px'],
-    base: ['16px', '24px'],
-    lg: ['20px', '28px'],
-    xl: ['24px', '32px'],
   },
   plugins: [],
 };
