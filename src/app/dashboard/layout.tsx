3bec8aae2e95b680572763709380072aca702cbc
Guli.Ahmadova <ahmedovaguli2001@gmail.com>
2024-07-10 15:51:24 +0400

    conflict

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,11 +5,7 @@ import ProtectRoute from "@/features/ProtectRoute";
 import { Suspense, useState } from "react";
 import Loading from "../loading";
 
-export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     const [openSidebar, setOpenSidebar] = useState(true);
 
     return (
@@ -17,7 +13,7 @@ export default function RootLayout({
             <Suspense fallback={<Loading />}>
                 <div className="flex h-screen bg-gray-100">
                     <Sidebar openSidebar={openSidebar} />
-                    <div className="flex flex-col flex-1 overflow-y-auto">
+                    <div className="flex flex-col flex-1 overflow-y-auto bg-surface-background">
                         <Header setOpenSidebar={setOpenSidebar} />
                         <div className="p-4">
                             {children}
