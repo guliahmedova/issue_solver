4684b7afd470b0016fb4d70dc7cbc03402a85d18
Guli.Ahmadova <ahmedovaguli2001@gmail.com>
2024-07-11 11:14:15 +0400

    auth loader

--- src/features/ChangePassword/index.tsx
+++ src/features/ChangePassword/index.tsx
@@ -17,6 +17,7 @@ const ChangePassword = () => {
   const { trigger: changePasswordTrigger } = useRequestMutation(API.reset_password, { method: 'POST' });
   const router = useRouter();
   const [error, setError] = useState(null);
+  const [loader, setLoader] = useState(false);
 
   const handleSubmit = async (values: FormValues, actions: FormikHelpers<FormValues>) => {
     const otpToken = sessionStorage.getItem('otp_token');
@@ -28,6 +29,8 @@ const ChangePassword = () => {
     };
 
     try {
+      setLoader(true);
+      setError(null);
       await changePasswordTrigger({
         body: data,
         params: { token: `${otpToken}` }
@@ -36,85 +39,93 @@ const ChangePassword = () => {
       sessionStorage.clear();
     } catch (error: any) {
       setError(error?.response?.data?.message);
+    } finally {
+      setLoader(false);
     }
   };
 
   return (
-    <Box className={style.changepassword_container} component="div">
-      <Box component="div" className={`${style.changepassword_content} lg:w-[68%] w-11/12 mx-auto`} >
-        <Box component="div">
-          <Typography className={`${style.form_title} select-none`}>Yeni şifrə təyin edin</Typography>
-          <Typography className={`${style.sub_title} select-none`}>
-            Daxil olmaq üçün yeni şifrə təyin edin
-          </Typography>
-          <Divider className={style.divider} component="hr" />
-        </Box>
+    <>
+      <Box className={style.changepassword_container} component="div">
+        <Box component="div" className={`${style.changepassword_content} lg:w-[68%] w-11/12 mx-auto`} >
+          <Box component="div">
+            <Typography className={`${style.form_title} select-none`}>Yeni şifrə təyin edin</Typography>
+            <Typography className={`${style.sub_title} select-none`}>
+              Daxil olmaq üçün yeni şifrə təyin edin
+            </Typography>
+            <Divider className={style.divider} component="hr" />
+          </Box>
 
-        <Box marginTop="40px">
-          <Formik
-            initialValues={{
-              password: "",
-              confirmPassword: "",
-            }}
-            onSubmit={handleSubmit}
-            validationSchema={toFormikValidationSchema(ValidationSchema)}
-            validateOnBlur={true}
-          >
-            {({
-              handleSubmit,
-              handleChange,
-              handleBlur,
-              values,
-              errors,
-              touched,
-              isValid,
-              dirty,
-            }: FormikProps<FormValues>) => (
-              <Form onSubmit={handleSubmit}>
-                <Field
-                  name="password"
-                  labelText="Şifrə"
-                  type="password"
-                  component={Input}
-                  placeholder="Şifrənizi təyin edin"
-                  autoComplete="password"
-                  errorText={touched.password && errors.password ? errors.password : undefined}
-                  value={values.password}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-                <Field
-                  name="confirmPassword"
-                  labelText="Şifrə təsdiqi"
-                  type="password"
-                  component={Input}
-                  placeholder="Şifrənizi təsdiq edin"
-                  autoComplete="confirmPassword"
-                  errorText={
-                    touched.confirmPassword && errors.confirmPassword
-                      ? errors.confirmPassword
-                      : undefined
-                  }
-                  value={values.confirmPassword}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-                {error && <Typography color="red">{error}</Typography>}
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="primary"
-                  sx={{ mb: 2, textTransform: "capitalize" }}
-                  disabled={!isValid || !dirty}
-                >
-                  Yenilə
-                </Button>
-              </Form>
-            )}
-          </Formik>
+          <Box marginTop="40px">
+            <Formik
+              initialValues={{
+                password: "",
+                confirmPassword: "",
+              }}
+              onSubmit={handleSubmit}
+              validationSchema={toFormikValidationSchema(ValidationSchema)}
+              validateOnBlur={true}
+            >
+              {({
+                handleSubmit,
+                handleChange,
+                handleBlur,
+                values,
+                errors,
+                touched,
+                isValid,
+                dirty,
+              }: FormikProps<FormValues>) => (
+                <Form onSubmit={handleSubmit}>
+                  <Field
+                    name="password"
+                    labelText="Şifrə"
+                    type="password"
+                    component={Input}
+                    placeholder="Şifrənizi təyin edin"
+                    autoComplete="password"
+                    errorText={touched.password && errors.password ? errors.password : undefined}
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  <Field
+                    name="confirmPassword"
+                    labelText="Şifrə təsdiqi"
+                    type="password"
+                    component={Input}
+                    placeholder="Şifrənizi təsdiq edin"
+                    autoComplete="confirmPassword"
+                    errorText={
+                      touched.confirmPassword && errors.confirmPassword
+                        ? errors.confirmPassword
+                        : undefined
+                    }
+                    value={values.confirmPassword}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {error && <Typography color="red">{error}</Typography>}
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="primary"
+                    sx={{ mb: 2, textTransform: "capitalize" }}
+                    disabled={!isValid || !dirty}
+                  >
+                    Yenilə
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </Box>
         </Box>
       </Box>
-    </Box>
+
+      <div className={`${loader ? 'fixed' : 'hidden'} top-0 bottom-0 lg:left-auto left-0 right-0 flex lg:w-[50%] flex-col items-center justify-center bg-black/10 z-40`}>
+        <CircularProgress size="4rem" />
+      </div>
+    </>
   );
 };
 
--- src/features/email/index.tsx
+++ src/features/email/index.tsx
@@ -18,96 +18,106 @@ export default function ForgotPassword() {
   const { trigger: sendEmailTrigger } = useRequestMutation(API.forgot_password, { method: 'POST' });
   const router = useRouter();
   const [emailError, setEmailError] = useState<null | string>(null);
+  const [loader, setLoader] = useState(false);
 
   const handleSubmit = async (values: FormValues, actions: FormikHelpers<FormValues>) => {
     try {
+      setLoader(true);
+      setEmailError(null);
       const email = values?.email;
       await sendEmailTrigger({ body: { email: email } });
       actions.setSubmitting(false);
       router.push('/confirm-otp');
       sessionStorage.setItem('user_email', email);
-      setEmailError(null);
     } catch (error: unknown) {
       if (error instanceof AxiosError) {
         setEmailError(error?.response?.data?.message);
       }
       actions.setSubmitting(false);
+    } finally {
+      setLoader(false);
     }
   };
 
   return (
-    <Box
-      width="100%"
-      height="100%"
-      display="flex"
-      justifyContent="center"
-      flexDirection="column"
-      alignItems="center"
-    >
-      <Box paddingBottom="20px" textAlign="left" className="lg:w-[68%] w-11/12 mx-auto">
-        <Typography color="initial" fontSize={28} fontWeight={600} className="select-none">
-          E-poçtunuzu daxil edin
-        </Typography>
-        <Typography fontSize={15} fontWeight={400} sx={{ color: "#9D9D9D" }} noWrap className="select-none">
-          E-poçt hesabınıza təsdiq kod göndəriləcək
-        </Typography>
-      </Box>
-      <Divider
-        component="hr"
-        color="#2981FF"
-        style={{
-          borderColor: "#2981ff",
-          opacity: "0.4",
-          width: "68%",
-          marginBottom: "30px",
-        }}
-      />
-
-      <Formik
-        initialValues={{
-          email: "",
-        }}
-        onSubmit={handleSubmit}
-        validationSchema={toFormikValidationSchema(EmailValidationSchema)}
-        validateOnBlur={true}
+    <>
+      <Box
+        width="100%"
+        height="100%"
+        display="flex"
+        justifyContent="center"
+        flexDirection="column"
+        alignItems="center"
       >
-        {({
-          handleSubmit,
-          handleChange,
-          handleBlur,
-          values,
-          errors,
-          touched,
-          isValid,
-          dirty,
-        }: FormikProps<FormValues>) => (
-          <Box display="flex" justifyContent="center" alignItems="center" className="lg:w-[68%] w-11/12 mx-auto">
-            <Form
-              onSubmit={handleSubmit}
-              style={{ width: "100%", display: "flex", flexDirection: "column", gap: "60px" }}
-            >
-              <Field
-                name="email"
-                labelText="E-poçt"
-                type="text"
-                autoFocus={true}
-                autoComplete="email"
-                component={Input}
-                placeholder="E-poçtunuzu daxil edin"
-                errorText={touched.email && errors.email ? errors.email : undefined}
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-              {emailError && <Typography color="red" textAlign="left" width="60%">{emailError}</Typography>}
+        <Box paddingBottom="20px" textAlign="left" className="lg:w-[68%] w-11/12 mx-auto">
+          <Typography color="initial" fontSize={28} fontWeight={600} className="select-none">
+            E-poçtunuzu daxil edin
+          </Typography>
+          <Typography fontSize={15} fontWeight={400} sx={{ color: "#9D9D9D" }} noWrap className="select-none">
+            E-poçt hesabınıza təsdiq kod göndəriləcək
+          </Typography>
+        </Box>
+        <Divider
+          component="hr"
+          color="#2981FF"
+          style={{
+            borderColor: "#2981ff",
+            opacity: "0.4",
+            width: "68%",
+            marginBottom: "30px",
+          }}
+        />
 
-              <Button variant="primary" type="submit" disabled={!isValid || !dirty}>
-                Təsdiq kodu göndər
-              </Button>
-            </Form>
-          </Box>
-        )}
-      </Formik>
-    </Box>
+        <Formik
+          initialValues={{
+            email: "",
+          }}
+          onSubmit={handleSubmit}
+          validationSchema={toFormikValidationSchema(EmailValidationSchema)}
+          validateOnBlur={true}
+        >
+          {({
+            handleSubmit,
+            handleChange,
+            handleBlur,
+            values,
+            errors,
+            touched,
+            isValid,
+            dirty,
+          }: FormikProps<FormValues>) => (
+            <Box display="flex" justifyContent="center" alignItems="center" className="lg:w-[68%] w-11/12 mx-auto">
+              <Form
+                onSubmit={handleSubmit}
+                style={{ width: "100%", display: "flex", flexDirection: "column", gap: "60px" }}
+              >
+                <Field
+                  name="email"
+                  labelText="E-poçt"
+                  type="text"
+                  autoFocus={true}
+                  autoComplete="email"
+                  component={Input}
+                  placeholder="E-poçtunuzu daxil edin"
+                  errorText={touched.email && errors.email ? errors.email : undefined}
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {emailError && <Typography color="red" textAlign="left" width="60%">{emailError}</Typography>}
+
+                <Button variant="primary" type="submit" disabled={!isValid || !dirty}>
+                  Təsdiq kodu göndər
+                </Button>
+              </Form>
+            </Box>
+          )}
+        </Formik>
+      </Box>
+
+      <div className={`${loader ? 'fixed' : 'hidden'} top-0 bottom-0 lg:left-auto left-0 right-0 flex lg:w-[50%] flex-col items-center justify-center bg-black/10 z-40`}>
+        <CircularProgress size="4rem" />
+      </div>
+    </>
   );
 };
