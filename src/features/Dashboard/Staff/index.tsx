c1528391f084a970762ec191f041f0c44dcf639f
Guli.Ahmadova <ahmedovaguli2001@gmail.com>
2024-07-17 16:47:37 +0400

    delete staff

--- src/features/Dashboard/Staff/index.tsx
+++ src/features/Dashboard/Staff/index.tsx
@@ -1,19 +1,29 @@
 "use client";
 import { plus, trashbin } from "@/assets/imgs";
 import API from "@/http/api";
-import { useRequest } from "@/http/request";
+import { useRequest, useRequestMutation } from "@/http/request";
+import { CircularProgress } from "@mui/material";
 import Image from "next/image";
 import { useState } from "react";
 import CreatePopup from "./CreatePopup";
 
 interface IStaff {
-    email: string;
-    fullName: string;
+    fullname: string;
+    username: string;
+    organizationName: string;
+    isActiveOrganization: boolean;
 };
 
 const Staff = () => {
-    const { data: staffs } = useRequest(API.staffs_get);
+    const { data: staffs, isLoading } = useRequest(API.staffs_get);
+    const { trigger: deleteStaffTrigger } = useRequestMutation(API.staff_delete, { method: 'DELETE' });
     const [openPopup, setOpenPopup] = useState(false);
+
+    const handleDeleteStaff = async (username: string) => {
+        const res = await deleteStaffTrigger({ params: username });
+        console.log("Response delete: ", res);
+        console.log("username: ", username);
+    };
 
     return (
         <>
@@ -35,17 +45,27 @@ const Staff = () => {
                     </div>
 
                     <div>
-                        {staffs?.data?.items?.map((item: IStaff, index: number) => (
-                            <div className="grid grid-cols-5 items-center justify-between bg-white py-6 px-8 rounded-xl mb-3" key={index}>
-                                <span className="text-xs select-none">{index + 1}</span>
-                                <span className="text-xs">{item.fullName}</span>
-                                <span className="text-xs">{item.fullName}</span>
-                                <span className="text-xs whitespace-nowrap">İnnovasiya və Rəqəmsal İnkşaf Agentliyi</span>
-                                <div className="flex justify-end">
-                                    <Image alt="" src={trashbin} className="cursor-pointer" />
+                        {
+                            isLoading ? (
+                                <div className={`fixed top-0 bottom-0 left-0 right-0 flex w-full flex-col items-center justify-center bg-black/10 z-40`}>
+                                    <CircularProgress size="4rem" />
                                 </div>
-                            </div>
-                        ))}
+                            ) : (
+                                staffs?.data?.items?.map((item: IStaff, index: number) => (
+                                    <div className="grid grid-cols-5 items-center justify-between bg-white py-6 px-8 rounded-xl mb-3" key={index}>
+                                        <span className="text-xs select-none">{index + 1}</span>
+                                        <span className="text-xs">{item.fullname}</span>
+                                        <span className="text-xs">{item.username}</span>
+                                        <span className="text-xs whitespace-nowrap text-center">{item.isActiveOrganization && item.organizationName}</span>
+                                        <div className="flex justify-end"
+                                            onClick={() => handleDeleteStaff(item.username)}
+                                        >
+                                            <Image alt="" src={trashbin} className="cursor-pointer" />
+                                        </div>
+                                    </div>
+                                ))
+                            )
+                        }
                     </div>
                 </div>
             </div>
