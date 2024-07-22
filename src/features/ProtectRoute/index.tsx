e44b89b5b0f5456a9220a8c2def6ef3deb1cdce3
Guli.Ahmadova <ahmedovaguli2001@gmail.com>
2024-07-22 11:10:04 +0400

    change api url

--- src/features/ProtectRoute/index.tsx
+++ src/features/ProtectRoute/index.tsx
@@ -54,8 +54,7 @@ export const sidebarMenu = [
 ];
 
 const ProtectRoute = ({ children }: IProtectRoute) => {
-  // const token = useAuthStore(state => state.authData?.token);
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbGtpbnN1bGV5bWFub3YyMDBAZ21haWwuY29tIiwiYXV0aG9yaXRpZXMiOlsiVVNFUiIsIkdPVkVSTUVOVCIsIlNVUEVSX1NUQUZGIiwiU1RBRkYiLCJBRE1JTiJdLCJuYmYiOjE3MTk0Nzg3NTYsImV4cCI6MjAzNTAxMTU0NSwiaXNzIjoiaWxraW5AU3VsZXltYW5vdi5jb20iLCJhdWQiOiJpbGtpbkBTdWxleW1hbm92LmNvbSJ9.QHD7oSGvy3h-zKMbhQvo1ieREFhW2ic_4WMaWVMtnL0"
+  const token = useAuthStore(state => state.authData?.token);
   const isLoading = useAuthStore(state => state.loading);
 
   if (isLoading || !token) {
